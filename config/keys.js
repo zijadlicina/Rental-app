ee05f3b68ad34e467ba1ff835aee077ce7fb3880
zijadlicina <zijad.licina1@gmail.com>
2022-04-11 13:48:51 +0200

    Server running & database connection

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://user1:antintern@rest-shop.nmg9j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoURI = require('./config/keys').mongoURI;
+
+const app = express();
+
+// Database connection
+mongoose.connect(mongoURI)
+    .then(() => console.log("MongoDB connected..."))
+    .catch((err) => console.log(err))
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
+
+
